663fd317084545d3e40bb9271d24c5797d5bda03
nanto7375 <jsu7375@gmail.com>
2021-09-15 22:43:23 +0900

    통계 출력

---
+++ src/MatchTest.java
@@ -0,0 +1,56 @@
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import java.util.*;
+
+class MatchTest {
+
+	@Test
+	void test() {
+	}
+
+	@Test
+	List convertToList() {
+		String str = "1,2,3,4,5,6";
+		StringTokenizer st = new StringTokenizer(str, ", ");
+
+		List list = new ArrayList(st.countTokens());		
+		while(st.hasMoreTokens()) {
+			list.add(Integer.parseInt(st.nextToken()));
+		}
+
+		return list;
+	}
+	
+	@Test
+	void setResult() {
+		int[] match = new int[7];
+		ArrayList lottos = new ArrayList(6);
+		for (int i = 0 ; i < 6 ; i++) {
+			match[getMatch((List)lottos.get(i))]++;
+		}
+	}
+
+	@Test
+	private int getMatch(List numbers) {
+		int count = 0;
+		for (int i = 0; i < numbers.size(); i++) {
+			count += detailCheck((int)numbers.get(i));
+		}
+
+		return count;
+	}
+
+	@Test
+	private int detailCheck(int number) {
+		List answer = new ArrayList();
+		answer.add(1);		answer.add(2);
+		answer.add(3);		answer.add(4);
+		answer.add(5);		answer.add(6);
+		if (answer.contains(number)) return 1;
+
+		return 0;		
+	}
+}
